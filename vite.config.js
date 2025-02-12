3210ce1ec97315d385bf4c62ff399fcdf35ea067
gal gordon <gal.gordon@gmail.com>
2025-02-12 20:35:15 -0300

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/deep-link/',  // Should just be the repository name
-  plugins: [react()],
-  // ... existing code ...
+  base: '/deep-link/'
 }) 
